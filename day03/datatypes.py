d4d9881b875135e8267a41b0f69d689cc9d5b603
Sohaib Ali <101839296+SohaibAli2007@users.noreply.github.com>
2025-07-04 13:30:06 -0400

    Add files via upload

---
+++ day03/datatypes.py
@@ -0,0 +1,12 @@
+word = "hi"
+print("hi")
+print(isinstance(word, str))
+
+number = 13
+print(isinstance(number, int))
+
+decimal = 2.0
+print(isinstance(decimal, float))
+
+numberConvert = "13"
+print(int(numberConvert))
